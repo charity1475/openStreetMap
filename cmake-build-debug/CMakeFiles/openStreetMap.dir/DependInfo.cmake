86041089e363373158cea8eb23a1fcd6fbecc1d6
Steinny <charitymbisi@gmail.com>
2021-04-07 17:18:49 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/openStreetMap.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/charity/CLionProjects/openStreetMap/src/main.cpp" "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/CMakeFiles/openStreetMap.dir/src/main.cpp.o"
+  "/home/charity/CLionProjects/openStreetMap/src/model.cpp" "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/CMakeFiles/openStreetMap.dir/src/model.cpp.o"
+  "/home/charity/CLionProjects/openStreetMap/src/render.cpp" "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/CMakeFiles/openStreetMap.dir/src/render.cpp.o"
+  "/home/charity/CLionProjects/openStreetMap/src/route_model.cpp" "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/CMakeFiles/openStreetMap.dir/src/route_model.cpp.o"
+  "/home/charity/CLionProjects/openStreetMap/src/route_planner.cpp" "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/CMakeFiles/openStreetMap.dir/src/route_planner.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/io2d"
+  "/usr/include/cairo"
+  "/usr/include/GraphicsMagick"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/openStreetMap.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openStreetMap.dir/src/main.cpp.o"
+  "CMakeFiles/openStreetMap.dir/src/model.cpp.o"
+  "CMakeFiles/openStreetMap.dir/src/render.cpp.o"
+  "CMakeFiles/openStreetMap.dir/src/route_model.cpp.o"
+  "CMakeFiles/openStreetMap.dir/src/route_planner.cpp.o"
+  "openStreetMap"
+  "openStreetMap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openStreetMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
