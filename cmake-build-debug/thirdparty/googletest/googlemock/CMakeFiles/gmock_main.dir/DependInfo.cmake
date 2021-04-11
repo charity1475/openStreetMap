b223e14785dcde5ac0992b7755bc89bfbc9e423c
Steinny <charitymbisi@gmail.com>
2021-04-12 00:08:13 +0300

    render

---
+++ cmake-build-debug/thirdparty/googletest/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/charity/CLionProjects/openStreetMap/thirdparty/googletest/googlemock/src/gmock_main.cc" "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/thirdparty/googletest/googlemock/CMakeFiles/gmock_main.dir/src/gmock_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../thirdparty/googletest/googletest/include"
+  "../thirdparty/googletest/googletest"
+  "../thirdparty/googletest/googlemock/include"
+  "../thirdparty/googletest/googlemock"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/thirdparty/googletest/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake"
+  "/home/charity/CLionProjects/openStreetMap/cmake-build-debug/thirdparty/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/thirdparty/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/charity/CLionProjects/openStreetMap/thirdparty/googletest/googletest
+# Build directory: /home/charity/CLionProjects/openStreetMap/cmake-build-debug/thirdparty/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
